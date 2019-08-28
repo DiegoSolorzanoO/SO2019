fbf9218401a8d7d3120145c0ed8f3ab75f544ce7
DiegoSolorzanoO <a01422048@itesm.mx>
2019-08-28 01:29:18 +0000

    docker2

--- hello/hello.c
+++ hello/hello.c
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-#include <stdio.h>
-
-int main(){
-    printf("Hello World\n");
-=======
 #include "./libhello.h"
 
 
@@ -12,6 +6,5 @@ int main(int argc, char **argv){
     for(i = 1; i < argc; i++) {
         printHello(argv[i]);
     }
->>>>>>> e67aaef57789f772e3129b7cc27fe486676bba7a
     return 0;
 }
