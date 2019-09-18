5d7c48c0483cb466c1f6a20fe5bc3872ad543d58
DiegoSolorzanoO <a01422048@itesm.mx>
2019-09-18 01:52:14 +0000

    Cambio de init

--- xv6-public/kill.c
+++ xv6-public/kill.c
@@ -12,6 +12,10 @@ main(int argc, char **argv)
     exit();
   }
   for(i=1; i<argc; i++)
-    kill(atoi(argv[i]));
+    if(atoi(argv[i])==1){
+        printf(1,"The process init cannot be interrupted \n");
+    } else {
+        kill(atoi(argv[i]));
+    }
   exit();
 }
